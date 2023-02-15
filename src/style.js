bbbe74b65b77a817cc186d131f939cb4bbd66b74
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-15 13:31:05 -0800

    Update style.js

--- src/style.js
+++ src/style.js
@@ -1,7 +1,3 @@
-
-
-
-
 let citySelect = document.querySelector("#city");
 citySelect.addEventListener("change", showTime);
 
@@ -10,26 +6,18 @@ let parisTime = moment()
   .format("dddd, MMMM, D, YYYY h:mm A");
 let parisElement = document.querySelector("#paris");
 
-
-
-let laTime = moment()
-  
-  .format("dddd, MMMM D, YYYY h:mm A");
+let laTime = moment().format("dddd, MMMM D, YYYY h:mm A");
 let laElement = document.querySelector("#los-angeles");
-let laDateElement= laElement.querySelector('.date');
-let laTimeElement = laElement.querySelector('.time')
-laDateElement.innerHTML = 
-  laTime.format("MMMM Do, YYYY ");
-laTimeElement.innerHTML = "h:mm A";
+let laDateElement = laElement.querySelector(".date");
+let laTimeElement = laElement.querySelector(".time");
+laDateElement.innerHTML = laTime.format("MMMM Do, YYYY ");
+laTimeElement.innerHTML = laTime.format("h:mm :ss [<small>]A[</small>]");
 
-let tokyoTime = moment()
-tz("Asia/Tokyo")
-.format("dddd, MMMM D, YYYY h:mm A");
+let tokyoTime = moment();
+tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
 let tokyoElement = document.querySelector("#tokyo");
 
-let zagrebTime = moment()
-  .tz("")
-  .format("dddd, MMMM D, YYYY h:mm A");
+let zagrebTime = moment().tz("").format("dddd, MMMM D, YYYY h:mm A");
 let zagrebElement = document.querySelector("#zagreb");
 
 let londonTime = moment();
