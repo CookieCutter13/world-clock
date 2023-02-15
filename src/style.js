6ad5d0394b9d65ec4d00df5e070afeabfd0bd507
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-15 12:51:59 -0800

    added java

--- src/style.js
+++ src/style.js
@@ -1,1 +1,26 @@
+let citySelect = document.querySelector("#cities");
+citySelect.addEventListener("change", showTime);
 
+let parisTime = moment()
+  .tz("Europe/Paris")
+  .format("dddd, MMMM, D, YYYY h:mm A");
+let parisElement = document.querySelector("#parisTime");
+
+let Time = moment()
+  .tz("")
+  .format("dddd, MMMM D, YYYY h:mm A");
+let Element = document.querySelector("#");
+
+let tokyoTime = moment()
+tz("Asia/Tokyo")
+.format("dddd, MMMM D, YYYY h:mm A");
+let tokyoElement = document.querySelector("#tokyo");
+
+let Time = moment()
+  .tz("")
+  .format("dddd, MMMM D, YYYY h:mm A");
+let Element = document.querySelector("#");
+
+let Time = moment();
+tz("").format("dddd, MMMM D, YYYY h:mm A");
+let Element = document.querySelector("#");
