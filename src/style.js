4aa57680f5d0c8315866c99d33c749581eab857b
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-15 12:45:35 -0800

    cut java

--- src/style.js
+++ src/style.js
@@ -1,12 +1,1 @@
-let parisTime = moment()
-  .tz("Europe/Paris")
-  .format("dddd, MMMM, D, YYYY h:mm A");
-let parisElement = document.querySelector("#parisTime");
 
-let sydneyTime = moment()
-  .tz("Australia/Sydney")
-  .format("dddd, MMMM D, YYYY h:mm A");
-let sydneyElement = document.querySelector("#sydney");
-
-let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
-let honoluluElement = document.querySelector("#tokyo");
