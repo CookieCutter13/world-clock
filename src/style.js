4fd3f97eec3506d3a012c47940f18635d71fecec
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-15 13:49:41 -0800

    ttime

--- src/style.js
+++ src/style.js
@@ -1,8 +1,3 @@
-let parisTime = moment()
-  .tz("Europe/Paris")
-  .format("dddd, MMMM, D, YYYY h:mm A");
-let parisElement = document.querySelector("#paris");
-
 let laElement = document.querySelector("#los-angeles");
 let laDateElement = laElement.querySelector(".date");
 let laTimeElement = laElement.querySelector(".time");
@@ -11,13 +6,34 @@ let laTime = moment().tz("America/Los_Angeles");
 laDateElement.innerHTML = laTime.format("MMMM Do, YYYY ");
 laTimeElement.innerHTML = laTime.format("h:mm :ss [<small>]A[</small>]");
 
-let tokyoTime = moment();
-tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
 let tokyoElement = document.querySelector("#tokyo");
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
 
-let zagrebTime = moment().tz("").format("dddd, MMMM D, YYYY h:mm A");
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do, YYYY ");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm :ss [<small>]A[</small>]");
+
 let zagrebElement = document.querySelector("#zagreb");
+let zagrebDateElement = Element.querySelector(".date");
+let xagrebTimeElement = Element.querySelector(".time");
+let zagrebTime = moment().tz("");
 
-let londonTime = moment();
-tz("").format("dddd, MMMM D, YYYY h:mm A");
-let londonElement = document.querySelector("#london");
+zagrebDateElement.innerHTML = Time.format("MMMM Do, YYYY ");
+zagrebTimeElement.innerHTML = Time.format("h:mm :ss [<small>]A[</small>]");
+
+let Element = document.querySelector("#");
+let DateElement = Element.querySelector(".date");
+let TimeElement = Element.querySelector(".time");
+let Time = moment().tz("");
+
+DateElement.innerHTML = Time.format("MMMM Do, YYYY ");
+TimeElement.innerHTML = Time.format("h:mm :ss [<small>]A[</small>]");
+
+let Element = document.querySelector("#los-angeles");
+let DateElement = Element.querySelector(".date");
+let TimeElement = Element.querySelector(".time");
+let Time = moment().tz("");
+
+DateElement.innerHTML = Time.format("MMMM Do, YYYY ");
+TimeElement.innerHTML = Time.format("h:mm :ss [<small>]A[</small>]");
