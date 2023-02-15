9baef885069fbd87cca2e25d40067def1bcbff03
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-15 13:38:04 -0800

    time

--- src/style.js
+++ src/style.js
@@ -6,10 +6,12 @@ let parisTime = moment()
   .format("dddd, MMMM, D, YYYY h:mm A");
 let parisElement = document.querySelector("#paris");
 
-let laTime = moment().tz("America/Los_Angeles");
+
 let laElement = document.querySelector("#los-angeles");
 let laDateElement = laElement.querySelector(".date");
 let laTimeElement = laElement.querySelector(".time");
+let laTime = moment().tz("America/Los_Angeles");
+
 laDateElement.innerHTML = laTime.format("MMMM Do, YYYY ");
 laTimeElement.innerHTML = laTime.format("h:mm :ss [<small>]A[</small>]");
 
