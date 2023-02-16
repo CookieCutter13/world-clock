467f70e8037b9efcb173b39215a9647719fb3790
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-16 10:52:43 -0800

    add code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 function updateTime() {
   let laElement = document.querySelector("#los-angeles");
+  if (laElement){}
   let laDateElement = laElement.querySelector(".date");
   let laTimeElement = laElement.querySelector(".time");
   let laTime = moment().tz("America/Los_Angeles");
 
   laDateElement.innerHTML = laTime.format("MMMM Do, YYYY ");
   laTimeElement.innerHTML = laTime.format("h:mm:ss [<small>]A[</small>]");
+} 
 
   let tokyoElement = document.querySelector("#tokyo");
+  if(tokyoElement) {
   let tokyoDateElement = tokyoElement.querySelector(".date");
   let tokyoTimeElement = tokyoElement.querySelector(".time");
   let tokyoTime = moment().tz("Asia/Tokyo");
 
   tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do, YYYY ");
   tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+
 
   let parisElement = document.querySelector("#paris");
+  if(parisElement){
   let parisDateElement = parisElement.querySelector(".date");
   let parisTimeElement = parisElement.querySelector(".time");
   let parisTime = moment().tz("Europe/Paris");
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do, YYYY ");
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
-
+  }
   let londonElement = document.querySelector("#london");
+  if(londonElement){
   let londonDateElement = londonElement.querySelector(".date");
   let londonTimeElement = londonElement.querySelector(".time");
   let londonTime = moment().tz("Europe/London");
