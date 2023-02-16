02c1813d3e7657aac30e567b9412be6128bbaa7f
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-16 11:00:34 -0800

    add code

--- src/index.js
+++ src/index.js
@@ -44,3 +44,28 @@ function updateTime() {
     );
   }
 }
+
+function updateCity(event) {
+  console.log(event);
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", "").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = `
+<div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss "
+        )}<small>${cityTime.format("A")}</small></div>
+      </div>`;
+}
+updateTime();
+setInterval(updateTime, 1000);
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
+
