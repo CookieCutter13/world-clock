b8a044b4055528c119116a084dbea77c9e411487
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-02-16 11:16:17 -0800

    add code

--- src/index.js
+++ src/index.js
@@ -56,11 +56,11 @@ function updateCity(event) {
 <div class="city">
         <div>
           <h2>${cityName}</h2>
-          <div class="date">${cityTime.format("MMM Do YYYY")}</div>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format(
           "h:mm:ss "
-        )}<small>${cityTime.format("A")}</small></div>
+        )}<small>${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
       </div>`;
 }
 updateTime();
